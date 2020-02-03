2f02ded174c8c77f3cedc206728502f9d90d273f
poisonedapple <s18515@salesio-sp.ac.jp>
2020-02-03 15:18:26 +0900

    Initial Commit

---
+++ search/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  search
+//
+//  Created by Kojima Minoru on 2020/02/03.
+//  Copyright © 2020 Kojima Minoru. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
