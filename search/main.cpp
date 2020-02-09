ca103f59a807bf97a8a7991c6d0ebf1f800b062d
poisonedapple <s18515@salesio-sp.ac.jp>
2020-02-10 01:39:21 +0900

    2 commit (入力とその表示	)

--- search/main.cpp
+++ search/main.cpp
@@ -10,6 +10,20 @@ using namespace std;
 //
 //};
 
+typedef struct agents{
+    int agentID;
+    int x;
+    int y;
+}agents;
+
+typedef struct teams{
+    int teamID;
+    agents agent[8];
+    int agcount;
+    int tilePoint;
+    int areaPoint;
+}teams;
+
 int main(int argc, const char * argv[]) {
     std::ifstream ifs("/Users/poisoned/all/search/search/field_turn0.json", ios::in);
     if (ifs.fail()) {
@@ -38,6 +52,51 @@ int main(int argc, const char * argv[]) {
             cout << field[j][i] << " ";
         }
         cout << endl;
+    }
+    int turn = obj["turn"].get<double>();
+    cout << endl << "turn : " << turn << endl;
+    vector<vector<int>> tiled(wth, vector<int>(hgh));
+    picojson::array tiles = obj["tiled"].get<picojson::array>();
+    for(int j = 0; j < hgh; j++){
+        picojson::array tiless = tiles[j].get<picojson::array>();
+        for (int i = 0; i < wth; i++) {
+            tiled[j][i] = tiless[i].get<double>();
+            cout << tiled[j][i] << " ";
+        }
+        cout << endl;
+    }
+    
+    teams team[2];
+    picojson::array tm = obj["teams"].get<picojson::array>();
+    int i = 0, is = 0;
+    for (picojson::array::iterator it = tm.begin(); it != tm.end(); it++){
+        is = 0;
+        picojson::object& o = it->get<picojson::object>();
+        picojson::array ag = o["agents"].get<picojson::array>();
+        for (picojson::array::iterator its = ag.begin(); its != ag.end(); its++){
+            picojson::object& e = its->get<picojson::object>();
+            team[i].agent[is].agentID = e["agentID"].get<double>();
+            team[i].agent[is].x = e["x"].get<double>();
+            team[i].agent[is].y = e["y"].get<double>();
+            is++;
+        }
+        team[i].agcount = is;
+        team[i].teamID = o["teamID"].get<double>();
+        team[i].tilePoint = o["tilePoint"].get<double>();
+        team[i].areaPoint = o["areaPoint"].get<double>();
+        i++;
+    }
+    for(int i = 0; i < 2; i++){
+        cout << endl << "team" << i << " : " << endl;
+        cout << "\tteamID : " << team[i].teamID << endl;
+        cout << "\t" << team[i].agcount << "agents : " << endl;
+        for(int j = 0; j < team[i].agcount; j++){
+            cout << "\t\tagentID : " << team[i].agent[j].agentID << endl;
+            cout << "\t\tx : " << team[i].agent[j].x << endl;
+            cout << "\t\ty : " << team[i].agent[j].y << endl << endl;
+        }
+        cout << "\ttilePoint : " << team[i].tilePoint << endl;
+        cout << "\tareaPoint : " << team[i].areaPoint << endl << endl;
     }
     return 0;
 }
